ac4d94fd4dfdac0ffcb0f7876278639e8203aee2
Timo Naumann <t.naumann@demandware.com>
2016-02-09 15:13:05 +0100

    created new testcase

---
+++ src/tests/cart/TCart_ValidatePersistentCart.java
@@ -0,0 +1,16 @@
+/*
+ * NOTE: This file is generated. Do not edit! Your changes will be lost.
+ */
+package tests.cart;
+import com.xceptance.xlt.api.engine.scripting.AbstractScriptTestCase;
+import com.xceptance.xlt.api.engine.scripting.ScriptName;
+
+
+/**
+ * <p>This Test case validate tha the shopping cart ist persistent after login logout</p>
+ */
+@ScriptName
+("tests.cart.TCart_ValidatePersistentCart")
+public class TCart_ValidatePersistentCart extends AbstractScriptTestCase
+{
+}
