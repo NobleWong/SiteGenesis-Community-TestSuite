04cfdb4d8d82eaaa460de70eeb8a72cecc804449
Timo Naumann <t.naumann@demandware.com>
2016-02-25 10:24:22 +0100

    added some comments and changed test case a little bit

--- src/tests/cart/TCart_ValidatePersistentCart.java
+++ src/tests/cart/TCart_ValidatePersistentCart.java
@@ -7,7 +7,32 @@ import com.xceptance.xlt.api.engine.scripting.ScriptName;
 
 
 /**
- * <p>This Test case validate tha the shopping cart ist persistent after login logout</p>
+ * <p>Test validate the persistent cart feature. It contains off two parts one for the persistent user cart and one for the site</p>
+ * <h1 id="setup-and-preparation">Setup and preparation</h1>
+ * <ul>
+ * <li>Start session, open homepage and delete all visible cookies.</li>
+ * <li>Create a new Account </li>
+ * <li>Search and add a product to the cart</li>
+ * <li>Store product data</li>
+ * </ul>
+ * <h1 id="scope">Scope</h1>
+ * <h2 id="persistent-user-cart">Persistent user cart</h2>
+ * <ul>
+ * <li>validate product in cart</li>
+ * <li>logout</li>
+ * <li>delete viseblie cookies</li>
+ * <li>login</li>
+ * <li>validate that product is still in cart</li>
+ * </ul>
+ * <h2 id="persistent-site-cart">Persistent site cart</h2>
+ * <ul>
+ * <li>open clean page</li>
+ * <li>Search Product and add to cart (the same as before ) </li>
+ * <li>validate cart</li>
+ * <li>delete cookies</li>
+ * <li>open site again</li>
+ * <li>validate cart</li>
+ * </ul>
  */
 @ScriptName
 ("tests.cart.TCart_ValidatePersistentCart")
