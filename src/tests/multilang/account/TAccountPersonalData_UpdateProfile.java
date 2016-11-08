dcb7d5a09cc379a54167e277f0f468c0ff0f09a4
tnaumannXCE <t.naumann@xceptance.net>
2016-11-08 09:59:53 +0100

    added multilang testcases for account, introduced new multilang modules

---
+++ src/tests/multilang/account/TAccountPersonalData_UpdateProfile.java
@@ -0,0 +1,31 @@
+/*
+ * NOTE: This file is generated. Do not edit! Your changes will be lost.
+ */
+package tests.multilang.account;
+import com.xceptance.xlt.api.engine.scripting.AbstractScriptTestCase;
+import com.xceptance.xlt.api.engine.scripting.ScriptName;
+
+
+/**
+ * <p>Test update profile data in account.</p>
+ * <h1 id="1-setup-and-preparation">1. Setup and preparation</h1>
+ * <ul>
+ * <li>Start session, open homepage and delete all visible cookies</li>
+ * <li>Create new account with profile data</li>
+ * </ul>
+ * <h1 id="2-scope-of-test">2. Scope of test</h1>
+ * <ul>
+ * <li>Login using the prepared account</li>
+ * <li>Update profile data (firstname, lastname and email)</li>
+ * <li>Logout via corresponding link on account page</li>
+ * <li>Login with new email</li>
+ * <li>Validate new profile data</li>
+ * <li>Change password</li>
+ * <li>Logout via corresponding link on account page</li>
+ * <li>Login with new password</li>
+ * </ul>
+ */
+@ScriptName("tests.multilang.account.TAccountPersonalData_UpdateProfile")
+public class TAccountPersonalData_UpdateProfile extends AbstractScriptTestCase
+{
+}
---
+++ src/tests/multilang/account/TAccount_CreateAccount.java
@@ -0,0 +1,31 @@
+/*
+ * NOTE: This file is generated. Do not edit! Your changes will be lost.
+ */
+package tests.multilang.account;
+import com.xceptance.xlt.api.engine.scripting.AbstractScriptTestCase;
+import com.xceptance.xlt.api.engine.scripting.ScriptName;
+
+
+/**
+ * <p>Test register profile with create account via link in header.</p>
+ * <h1 id="1-setup-and-preparation">1. Setup and preparation</h1>
+ * <ul>
+ * <li>Start session, open homepage and delete all visible cookies, create email and password</li>
+ * </ul>
+ * <h1 id="2-scope-of-test">2. Scope of test</h1>
+ * <ul>
+ * <li>Click register link in the header and goto the create account page</li>
+ * <li>Enter profile data and submit the form</li>
+ * <li>Logout using the link in the account area</li>
+ * <li>Validate logout via user menu panel header</li>
+ * <li>Login again to validate login data</li>
+ * <li>Validate login via user menu panel header</li>
+ * <li>Validate personal profile data</li>
+ * <li>Logout using the global link in the header</li>
+ * <li>Validate logout via user menu panel header</li>
+ * </ul>
+ */
+@ScriptName("tests.multilang.account.TAccount_CreateAccount")
+public class TAccount_CreateAccount extends AbstractScriptTestCase
+{
+}
